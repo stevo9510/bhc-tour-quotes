d1bc4ba2943843e953c9c9b0977010b7d7ab1455
stevo9510 <stevo9510@gmail.com>
2018-06-23 23:47:51 -0400

    wiring up duration logic

--- src/bhc/tours/BhcTourFrame.java
+++ src/bhc/tours/BhcTourFrame.java
@@ -6,12 +6,17 @@ import java.awt.Desktop;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import javax.swing.BorderFactory;
 import javax.swing.ButtonGroup;
@@ -27,11 +32,25 @@ import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
 import javax.swing.border.Border;
 import javax.swing.border.TitledBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 public class BhcTourFrame extends JFrame {
 	
 	private static final String BHC_HOME_PAGE_URL = "https://web7.jhuep.com/~sande107/bhc_site_v1/Homework3.html";
 	
+	// Note: These are based off BookingDay.java
+	private static final int VALID_START_YEAR = 2007;
+	private static final int VALID_END_YEAR = 2020;
+
+	private static final List<Integer> LAKE_DURATIONS = 
+			Collections.unmodifiableList(Arrays.asList(3, 5));
+	private static final List<Integer> PLATEAU_DURATIONS = 
+			Collections.unmodifiableList(Arrays.asList(2, 3, 4));
+	private static final List<Integer> PATH_DURATIONS = 
+			Collections.unmodifiableList(Arrays.asList(5, 7));
+	
+	private JComboBox durationComboBox;
 	
 	public BhcTourFrame() {
 		setTitle("BHC Tour Options");
@@ -42,10 +61,12 @@ public class BhcTourFrame extends JFrame {
 		mainPanel.setBorder(BorderFactory.createTitledBorder(panelBorder, "Tour Date", TitledBorder.TOP, TitledBorder.CENTER));
 		
 		getContentPane().add(mainPanel);
-				
-		JRadioButton lakeButton = createStandardRadioButton("Gardiner Lake", "gardiner-lake.jpg", "gardiner-lake_border.jpg");
-		JRadioButton plateauButton = createStandardRadioButton("Hellroaring Plateau", "plateau.jpg", "plateau_border.jpg");
-		JRadioButton pathButton = createStandardRadioButton("The Beaten Path", "beaten-path.jpg", "beaten-path_border.jpg");
+		
+		durationComboBox = new JComboBox();
+		
+		JRadioButton lakeButton = createStandardTrailRadioButton("Gardiner Lake", "gardiner-lake.jpg", "gardiner-lake_border.jpg", LAKE_DURATIONS);
+		JRadioButton plateauButton = createStandardTrailRadioButton("Hellroaring Plateau", "plateau.jpg", "plateau_border.jpg", PLATEAU_DURATIONS);
+		JRadioButton pathButton = createStandardTrailRadioButton("The Beaten Path", "beaten-path.jpg", "beaten-path_border.jpg", PATH_DURATIONS);
 		ButtonGroup radioButtonGroup = new ButtonGroup();
 		radioButtonGroup.add(lakeButton);
 		radioButtonGroup.add(plateauButton);
@@ -55,7 +76,7 @@ public class BhcTourFrame extends JFrame {
 		mainPanel.add(plateauButton, "Hellroaring Plateau");
 		mainPanel.add(pathButton, "The Beaten Path");
 		
-		JButton openWebsiteButton = new JButton("View More Details");
+		JButton openWebsiteButton = new JButton("View More Details...");
 		openWebsiteButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent evt) {
@@ -71,19 +92,42 @@ public class BhcTourFrame extends JFrame {
 		
 		JLabel durationLabel = createStandardLabel("Duration");
 		
+		
 		JComboBox monthComboBox = new JComboBox();
+		
+		JComboBox dayComboBox = createDayComboBox();
+		JComboBox yearComboBox = createYearComboBox();
 		monthComboBox.addItem("January");
 		
 		mainPanel.add(durationLabel);
+		mainPanel.add(durationComboBox);
 		mainPanel.add(monthComboBox);
+		mainPanel.add(dayComboBox);
+		mainPanel.add(yearComboBox);
+	}
+		
+	private JComboBox createDayComboBox() {
+		JComboBox dayComboBox = new JComboBox();
+		for(int dayIndex = 0; dayIndex < 31; dayIndex++) {
+			dayComboBox.addItem(dayIndex + 1);
+		}
+		return dayComboBox;
 	}
 	
+	private JComboBox createYearComboBox() {
+		JComboBox yearComboBox = new JComboBox();
+		for(int yearIndex = VALID_START_YEAR; yearIndex < VALID_END_YEAR + 1; yearIndex++) {
+			yearComboBox.addItem(yearIndex);
+		}		
+		return yearComboBox;
+	}
+		
 	private JLabel createStandardLabel(String text) {
 		JLabel label = new JLabel(text + ":");
 		return label;		
 	}
-	
-	private JRadioButton createStandardRadioButton(String text, String deselectedIconPath, String selectedIconPath) {
+		
+	private JRadioButton createStandardTrailRadioButton(String text, String deselectedIconPath, String selectedIconPath, List<Integer> durationOptions) {
 		
 		Icon deselectedIcon = new ImageIcon(getClass().getResource(deselectedIconPath));
 		Icon selectedIcon = new ImageIcon(getClass().getResource(selectedIconPath));
@@ -92,8 +136,23 @@ public class BhcTourFrame extends JFrame {
 		radioButton.setVerticalTextPosition(SwingConstants.TOP);
 		radioButton.setSelectedIcon(selectedIcon);
 		radioButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		radioButton.addItemListener(new ItemListener() {
+			@Override
+			public void itemStateChanged(ItemEvent evt) {
+				if(evt.getStateChange() == ItemEvent.SELECTED) {
+					addDurationOptionsToList(durationOptions);	
+				}				
+			}
+		});
 		return radioButton;
 	}
+
+	private void addDurationOptionsToList(List<Integer> durationOptions) {
+		durationComboBox.removeAllItems();
+		for(int durationOption : durationOptions) {
+			durationComboBox.addItem(durationOption);
+		}
+	}			
 	
 	// TODO: Utility Method
 	private static boolean openWebpage(URL url) {
@@ -130,5 +189,5 @@ public class BhcTourFrame extends JFrame {
 	    };
 		SwingUtilities.invokeLater(showFrame);
     }
-    
+       
 }
